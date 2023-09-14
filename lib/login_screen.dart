c6ee70412c7bce0e2e31e4f7493e23863ea1d2c5
Gogula Rajasekhar <iosrajasekhar@gmail.com>
2023-09-14 16:04:59 +0530

    Scroll and Login UI

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -14,53 +14,193 @@ class LoginScreen extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.all(20),
         color: const Color.fromARGB(255, 187, 124, 170),
-        child: Column(
-          children: [
-            const TextField(
-              decoration: InputDecoration(
-                  hintText: "User name", prefixIcon: Icon(Icons.email)),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            TextFormField(
-              textAlign: TextAlign.left,
-              obscureText: true,
-              decoration: const InputDecoration(
-                filled: true,
-                fillColor: Color.fromARGB(255, 180, 196, 241),
-                border: InputBorder.none,
-                hintText: "Password",
-                prefixIcon: Icon(Icons.lock),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const TextField(
+                decoration: InputDecoration(
+                    hintText: "User name", prefixIcon: Icon(Icons.email)),
               ),
-              validator: (value) {
-                if (value != null) {
-                  if (value.length > 3) {
-                    //ok
-                    return value;
+              const SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                textAlign: TextAlign.left,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 180, 196, 241),
+                  border: InputBorder.none,
+                  hintText: "Password",
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                validator: (value) {
+                  if (value != null) {
+                    if (value.length > 3) {
+                      //ok
+                      return value;
+                    } else {
+                      return "Error password";
+                    }
                   } else {
-                    return "Error password";
+                    return null;
                   }
-                } else {
-                  return null;
-                }
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: SizedBox(
-                width: double.infinity,
-                height: 50.0,
-                child: ElevatedButton(
-                  style: const ButtonStyle(
-                    alignment: Alignment.center,
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 50.0,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(
+                      alignment: Alignment.center,
+                    ),
+                    onPressed: () {},
+                    child: const Text("Submit"),
                   ),
-                  onPressed: () {},
-                  child: const Text("Submit"),
+                ),
+              )
+            ,
+             const TextField(
+                decoration: InputDecoration(
+                    hintText: "User name", prefixIcon: Icon(Icons.email)),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                textAlign: TextAlign.left,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 180, 196, 241),
+                  border: InputBorder.none,
+                  hintText: "Password",
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                validator: (value) {
+                  if (value != null) {
+                    if (value.length > 3) {
+                      //ok
+                      return value;
+                    } else {
+                      return "Error password";
+                    }
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 50.0,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(
+                      alignment: Alignment.center,
+                    ),
+                    onPressed: () {},
+                    child: const Text("Submit"),
+                  ),
                 ),
               ),
-            )
-          ],
+                          const TextField(
+                decoration: InputDecoration(
+                    hintText: "User name", prefixIcon: Icon(Icons.email)),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                textAlign: TextAlign.left,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 180, 196, 241),
+                  border: InputBorder.none,
+                  hintText: "Password",
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                validator: (value) {
+                  if (value != null) {
+                    if (value.length > 3) {
+                      //ok
+                      return value;
+                    } else {
+                      return "Error password";
+                    }
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 50.0,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(
+                      alignment: Alignment.center,
+                    ),
+                    onPressed: () {},
+                    child: const Text("Submit"),
+                  ),
+                ),
+              ),
+                          const TextField(
+                decoration: InputDecoration(
+                    hintText: "User name", prefixIcon: Icon(Icons.email)),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                textAlign: TextAlign.left,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 180, 196, 241),
+                  border: InputBorder.none,
+                  hintText: "Password",
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                validator: (value) {
+                  if (value != null) {
+                    if (value.length > 3) {
+                      //ok
+                      return value;
+                    } else {
+                      return "Error password";
+                    }
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 50.0,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(
+                      alignment: Alignment.center,
+                    ),
+                    onPressed: () {},
+                    child: const Text("Submit"),
+                  ),
+                ),
+              )
+            
+            
+            
+        
+        
+            ],
+          ),
         ),
       ),
     );
