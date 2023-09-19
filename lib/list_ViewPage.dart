8fc05d42ac918527a74746755a613c245088f4a2
Gogula Rajasekhar <iosrajasekhar@gmail.com>
2023-09-19 06:33:41 +0530

    static list view

--- lib/list_ViewPage.dart
+++ lib/list_ViewPage.dart
@@ -12,24 +12,29 @@ class ListViewPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("List view sample"),
       ),
-      body: ListView(
-        children: [
-          Container(
-            height: 100, color: Colors.amber,
-          ),
-           Container(
-            height: 100, color: const Color.fromARGB(255, 43, 42, 39),
-          ),
-           Container(
-            height: 100, color: Color.fromARGB(255, 20, 154, 195),
-          ),
-           Container(
-            height: 100, color: Color.fromARGB(255, 233, 67, 94),
-          ),
-           Container(
-            height: 100, color: Color.fromARGB(255, 56, 240, 160),
-          )
-        ],
+      body: SizedBox(
+        child: ListView(
+          padding: const EdgeInsets.all(20),
+          itemExtent: 200,
+          scrollDirection: Axis.vertical,
+          children: [
+            Container(
+              height: 100, color: Colors.amber,
+            ),
+             Container(
+              height: 100, color: const Color.fromARGB(255, 43, 42, 39),
+            ),
+             Container(
+              height: 100, color: Color.fromARGB(255, 20, 154, 195),
+            ),
+             Container(
+              height: 100, color: Color.fromARGB(255, 233, 67, 94),
+            ),
+             Container(
+              height: 100, color: Color.fromARGB(255, 56, 240, 160),
+            )
+          ],
+        ),
       ),
     );
   }
